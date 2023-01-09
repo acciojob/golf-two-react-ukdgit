ffe299820e844bf17dab4e84e688986b0a558b9e
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-09 17:42:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,8 +9,44 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({ left: "0px", top: '0px' })
+  };
+  const renderChoice = () => {
+    if (renderBall === false) {
+      return <button onClick={() => setRenderBall(true)} className="start" >Start</button>
+    }
+    else if (renderBall === true) {
+      return <div className="ball" style={ballPosition}></div>
+    }
+  };
+
+ useEffect(() => {
+    // initiate the event handler
+    window.addEventListener("keydown", (e) => {
+      if (e.key === 'ArrowRight') {
+        setBallPosition({ left: x + 5 + "px", top: y + "px" });
+        setX(x + 5)
+      }
+      if (e.key === 'ArrowUp') {
+        setBallPosition({ left: x + "px", top: y - 5 + "px" });
+        setY(y - 5)
+      }
+      if (e.key === 'ArrowLeft') {
+        setBallPosition({ left: x - 5 + "px", top: y + "px" });
+        setX(x - 5)
+      }
+      if (e.key === 'ArrowDown') {
+        setBallPosition({ left: x + "px", top: y + 5 + "px" });
+        setY(y + 5)
+      }
+    },  false);
+
+    
+  });
 
   return (
     <div className="playground">
